3a64d6a79ccda113f8c2d1d643b3d63d30d6c88e
aman khan <amankhan831986@gmail.com>
2026-04-20 17:05:18 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Admin from './Dashboard/Admin'
 import Employee from './Dashboard/Employee'
 import { authContext } from './Auth/Auth'
 
+saveToLocalStorage();
 
 const App = () => {
   const data = useContext(authContext);
